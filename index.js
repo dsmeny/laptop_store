c10fd7c660cd06ad38124b14c29a001265e22f0e
brad <bradley@dsmeny.com>
2019-05-03 20:03:30 -0400

    added nodemon and config node server/listener

---
+++ index.js
@@ -0,0 +1,19 @@
+const fs = require("fs");
+const http = require("http");
+const url = require("url");
+
+const json = fs.readFileSync(`${__dirname}/data/data.json`, "utf-8");
+
+const laptopData = JSON.parse(json);
+
+const server = http.createServer((req, res) => {
+  const query = url.parse(req.url, true);
+
+  console.log(query);
+  res.writeHead(200, { "Content-type": "text/html" });
+  res.end("This is the response!");
+});
+
+server.listen(3000, "127.0.0.1", () => {
+  console.log("listening for requests now");
+});
